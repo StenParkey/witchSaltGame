dec7a59c5c67ce472532dab4bfedea1057df4723
dogsalt_ <jpstennes@gmail.com>
2025-05-02 10:17:39 -0600

    added comma on line 20 of savedusers.js

--- resources/js/witchsaltapps/savedusers.js
+++ resources/js/witchsaltapps/savedusers.js
@@ -1,46 +1,98 @@
-let wLUsers = {
-    dogsalt_: [],
-    SailorPunk: [],
-    WildLerch: [],
-    CryptSeeker: [],
-    Domnablob: []
-}
-
-
-let savedusername; // Corrected variable name to match usage in other functions
-function getusername(username) {
-  const acceptedUsernames = [
-    'dogsalt_', 
-    'SailorPunk', 
-    'WildLerch', 
-    'CryptSeeker', 
-    'Domnablob', 
-    'Empress'];
-  let userNameInput;
-
-  if (username === acceptedUsernames[0]) {
-      userNameInput = acceptedUsernames[0];
-      console.log('Welcome to the Apartment my friend');
-  } else if (username === acceptedUsernames[1]) {
-      userNameInput = acceptedUsernames[1];
-      console.log('Welcome to the Apartment young nephew');
-  } else if (username === acceptedUsernames[2]) {
-      userNameInput = acceptedUsernames[2];
-      console.log('Welcome to the Apartment Tyrael of Skornhall');
-  } else if (username === acceptedUsernames[3]) {
-      userNameInput = acceptedUsernames[3];
-      console.log('Welcome to the Apartment søstern min!');
-  } else if (username === acceptedUsernames[4]) {
-      userNameInput = acceptedUsernames[4];
-      console.log('Welcome to the Apartment bestie!!!!');
-  } else if (username === acceptedUsernames[5]) {
-      userNameInput = acceptedUsernames[5];
-      console.log('Welcome to our Apartment Witchy Empress Bestie!!!!');
-  } else {
-      console.log('You must input a valid User Name!');
-      return;
-  }
-
-  savedusername = userNameInput; // Update the global variable
-  return userNameInput;
+let whiteListedUsers = {
+    dogsalt_: {
+        name: 'dogsalt_',
+        class: 'Deep Witch',
+        status: {
+            health: 100,
+            energy: 100,
+            sanity: 100
+        },
+        stats: {
+            strength: 0,
+            dexterity: 0,
+            constitution: 0,
+            charisma: 0,
+            wisdom: 0,
+            intelligence: 0,
+            luck: 0
+        }
+    },
+    WildLerch: {
+        name: 'dogsalt_',
+        class: 'Deep Witch',
+        status: {
+            health: 100,
+            energy: 100,
+            sanity: 100
+        },
+        stats: {
+            strength: 0,
+            dexterity: 0,
+            constitution: 0,
+            charisma: 0,
+            wisdom: 0,
+            intelligence: 0,
+            luck: 0
+        }
+    },
+    Domnablob: {
+        name: 'dogsalt_',
+        class: 'Deep Witch',
+        status: {
+            health: 100,
+            energy: 100,
+            sanity: 100
+        },
+        stats: {
+            strength: 0,
+            dexterity: 0,
+            constitution: 0,
+            charisma: 0,
+            wisdom: 0,
+            intelligence: 0,
+            luck: 0,
+            l
+        }
+    },
+    CryptSeeker: {
+        name: 'dogsalt_',
+        class: 'Deep Witch',
+        status: {
+            health: 100,
+            energy: 100,
+            sanity: 100,
+        },
+        stats: {
+            strength: 0,
+            dexterity: 0,
+            constitution: 0,
+            charisma: 0,
+            wisdom: 0,
+            intelligence: 0,
+            luck: 0,
+            l
+        }
+    },
+    SailorPunk: {
+        name: 'dogsalt_',
+        class: 'Deep Witch',
+        status: {
+            health: 100,
+            energy: 100,
+            sanity: 100,
+        },
+        stats: {
+            strength: 0,
+            dexterity: 0,
+            constitution: 0,
+            charisma: 0,
+            wisdom: 0,
+            intelligence: 0,
+            luck: 0,
+            l
+        }
+    }
+    
+    
+    
 }
